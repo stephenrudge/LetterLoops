4054bd89a01c410767623c35be75d88bfb6062ea
Stephen Rudge <sbrudge@comcast.net>
2023-02-03 19:27:20 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+string UserInput = Console.ReadLine();
+string Looped = "";
+int counter = 0;
+
+foreach (char i in UserInput)
+{
+    counter++;
+
+    char CapitalLetter = char.ToUpper.(i);
+
+    string letters = new string(i, counter - 1);
+
+    Looped += CapitalLetter + letters + ",";
+
+}
+
+Looped = Looped.Remove(Looped.Length - 1, 1);
+Console.WriteLine(Looped);
